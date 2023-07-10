23ef976fcb4fae631b498b3e3b078817f8505fe8
Amir Kooshky <kooshkya@gmail.com>
2023-07-10 16:38:13 +0330

    change model: separate click and View into independent models

--- advertiser_management/models.py
+++ advertiser_management/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import django.utils.timezone
 
 
 # Create your models here.
@@ -15,6 +16,16 @@ class Ad(models.Model):
     title = models.TextField()
     imgUrl = models.TextField()
     link = models.TextField()
-    clicks = models.IntegerField(default=0)
-    views = models.IntegerField(default=0)
-    advertiser = models.ForeignKey(to="Advertiser", on_delete=models.SET_NULL, null=True, related_name="ad_set")
+    advertiser = models.ForeignKey(to="Advertiser", on_delete=models.CASCADE, null=True, related_name="ads")
+
+
+class Click(models.Model):
+    click_time = models.DateTimeField(default=django.utils.timezone.now)
+    clicker_ip = models.GenericIPAddressField()
+    ad = models.ForeignKey(to="Ad", on_delete=models.CASCADE)
+
+
+class ViewEvent(models.Model):
+    view_time = models.DateTimeField(default=django.utils.timezone.now)
+    view_ip = models.GenericIPAddressField()
+    ad = models.ForeignKey(to="Ad", on_delete=models.CASCADE)
